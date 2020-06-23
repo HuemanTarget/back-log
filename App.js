41b3e3eb2751114d8ab9fb162d30a14b94a3dd81
Joshua Basche <pompouspro13@Joshuas-MacBook-Pro.local>
2020-06-22 22:47:15 -0700

    add loginscreen

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+} from "react-native";
+import { Provider } from "react-redux";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import store from "./redux/store";
+import BookWorm from "./BookWorm";
 
+import WelcomeScreen from "./screens/AppSwitchNavigator/WelcomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import LoadingScreen from "./screens/AppSwitchNavigator/LoadingScreen";
+import BooksReadingScreen from "./screens/HomeTabNavigator/BooksReadingScreen";
+import BooksReadScreen from "./screens/HomeTabNavigator/BooksReadScreen";
+
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createStackNavigator,
+  createDrawerNavigator,
+  createBottomTabNavigator,
+} from "react-navigation";
+
+import { Ionicons } from "@expo/vector-icons";
+import CustomDrawerComponent from "./screens/DrawerNavigator/CustomDrawerComponent";
+import colors from "./assets/colors";
 import * as firebase from "firebase/app";
 import { firebaseConfig } from "./config/config";
+
+import BooksCountContainer from "./redux/containers/BooksCountCountainer";
 
 class App extends Component {
   constructor() {
@@ -18,20 +51,157 @@ class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <LoginScreen />
+      </Provider>
     );
   }
 }
+const LoginStackNavigator = createStackNavigator(
+  {
+    WelcomeScreen: {
+      screen: WelcomeScreen,
+      navigationOptions: {
+        header: null,
+        headerBackTitle: null,
+      },
+    },
+    LoginScreen: {
+      screen: LoginScreen,
+      navigationOptions: {},
+    },
+  },
+  {
+    mode: "modal",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: colors.bgMain,
+      },
+    },
+  }
+);
+
+const HomeTabNavigator = createBottomTabNavigator(
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Total Books",
+        tabBarIcon: ({ tintColor }) => (
+          <BooksCountContainer color={tintColor} type="books" />
+        ),
+      },
+    },
+    BooksReadingScreen: {
+      screen: BooksReadingScreen,
+      navigationOptions: {
+        tabBarLabel: "Books Reading",
+        tabBarIcon: ({ tintColor }) => (
+          <BooksCountContainer color={tintColor} type="booksReading" />
+        ),
+      },
+    },
+    BooksReadScreen: {
+      screen: BooksReadScreen,
+      navigationOptions: {
+        tabBarLabel: "Books Read",
+        tabBarIcon: ({ tintColor }) => (
+          <BooksCountContainer color={tintColor} type="booksRead" />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.bgMain,
+      },
+      activeTintColor: colors.logoColor,
+      inactiveTintColor: colors.bgTextInput,
+    },
+  }
+);
+
+HomeTabNavigator.navigationOptions = ({ navigation }) => {
+  const { routeName } = navigation.state.routes[navigation.state.index];
+
+  switch (routeName) {
+    case "HomeScreen":
+      return {
+        headerTitle: "Total Books",
+      };
+    case "BooksReadingScreen":
+      return {
+        headerTitle: "Books Reading",
+      };
+    case "BooksReadScreen":
+      return {
+        headerTitle: "Books Read",
+      };
+    default:
+      return {
+        headerTitle: "Book Worm",
+      };
+  }
+};
+
+const HomeStackNavigator = createStackNavigator(
+  {
+    HomeTabNavigator: {
+      screen: HomeTabNavigator,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerLeft: (
+            <Ionicons
+              name="ios-menu"
+              size={30}
+              color={colors.logoColor}
+              onPress={() => navigation.openDrawer()}
+              style={{ marginLeft: 10 }}
+            />
+          ),
+        };
+      },
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: colors.bgMain,
+      },
+      headerTintColor: colors.txtWhite,
+    },
+  }
+);
+
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    HomeStackNavigator: {
+      screen: HomeStackNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerIcon: () => <Ionicons name="ios-home" size={24} />,
+      },
+    },
+    SettingsScreen: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        title: "Settings",
+        drawerIcon: () => <Ionicons name="ios-settings" size={24} />,
+      },
+    },
+  },
+  {
+    contentComponent: CustomDrawerComponent,
+  }
+);
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen,
+  LoginStackNavigator,
+  AppDrawerNavigator,
+});
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
