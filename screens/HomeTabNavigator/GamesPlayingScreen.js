e9c2ffec571cfb91e4df1fc8516fceb1bc76902e
Joshua Basche <pompouspro13@Joshuas-MacBook-Pro.local>
2020-06-23 14:31:17 -0700

    add Drawer and Home Tab Navigator

---
+++ screens/HomeTabNavigator/GamesPlayingScreen.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import colors from "../../assets/colors";
+import ListItem from "../../components/ListItem";
+import { connect } from "react-redux";
+import ListEmptyComponent from "../../components/ListEmptyComponent";
+
+class GamesPlayingScreen extends Component {
+  renderItem = (item) => {
+    return <ListItem item={item} />;
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: colors.bgMain }}>
+        {this.props.games.isLoadingGames && (
+          <View
+            style={{
+              ...StyleSheet.absoluteFill,
+              alignItems: "center",
+              justifyContent: "center",
+              zIndex: 1000,
+              elevation: 1000,
+            }}
+          >
+            <ActivityIndicator size="large" color={colors.logoColor} />
+          </View>
+        )}
+        <FlatList
+          data={this.props.games.gamesPlaying}
+          renderItem={({ item }, index) => this.renderItem(item, index)}
+          keyExtractor={(item, index) => index.toString()}
+          ListEmptyComponent={
+            !this.props.isLoadingGames && (
+              <View style={{ marginTop: 50, alignItems: "center" }}>
+                <Text style={{ fontWeight: "bold", color: colors.txtWhite }}>
+                  You Are Currently Not Playing Any Games
+                </Text>
+              </View>
+            )
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    games: state.games,
+  };
+};
+
+export default connect(mapStateToProps)(GamesPlayingScreen);
