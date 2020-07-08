716549a523795799afacaa32a61538ad48717da0
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-07-08 13:13:24 -0700

    add logic to forgot password screen

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -1,12 +1,102 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { Component, Fragment } from "react";
+import { Text, SafeAreaView, View, StyleSheet } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import FormInput from "../components/FormInput";
+import FormButton from "../components/FormButton";
+import ErrorMessage from "../components/ErrorMessage";
 
-const ForgotPasswordScreen = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Forgot Password Screen</Text>
-    </View>
-  );
-};
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .label("Email")
+    .email("Enter a valid email")
+    .required("Please enter a registered email"),
+});
+
+class ForgotPasswordScreen extends Component {
+  handlePasswordReset = async (values, actions) => {
+    const { email } = values;
+
+    firebase
+      .auth()
+      .sendPasswordResetEmail(email)
+      .then(function (user) {
+        alert("Please check your email...");
+      });
+    this.props.navigation.navigate("LoginScreen").catch(function (e) {
+      console.log(e);
+    });
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>Forgot Password?</Text>
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={(values, actions) => {
+            this.handlePasswordReset(values, actions);
+          }}
+          validationSchema={validationSchema}
+        >
+          {({
+            handleChange,
+            values,
+            handleSubmit,
+            errors,
+            isValid,
+            touched,
+            handleBlur,
+            isSubmitting,
+          }) => (
+            <Fragment>
+              <FormInput
+                name="email"
+                value={values.email}
+                onChangeText={handleChange("email")}
+                placeholder="Enter email"
+                autoCapitalize="none"
+                iconName="ios-mail"
+                iconColor="#2C384A"
+                onBlur={handleBlur("email")}
+              />
+              <ErrorMessage errorValue={touched.email && errors.email} />
+              <View style={styles.buttonContainer}>
+                <FormButton
+                  buttonType="outline"
+                  onPress={handleSubmit}
+                  title="Send Email"
+                  buttonColor="#039BE5"
+                  disabled={!isValid || isSubmitting}
+                />
+              </View>
+              <ErrorMessage errorValue={errors.general} />
+            </Fragment>
+          )}
+        </Formik>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 150,
+  },
+  text: {
+    color: "#333",
+    fontSize: 24,
+    marginLeft: 25,
+  },
+  buttonContainer: {
+    margin: 25,
+  },
+});
 
 export default ForgotPasswordScreen;
